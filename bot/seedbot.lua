5a06897cc7448bcee9a99f7b63063f41cbfb760f
sepehryellow <sepehr.habashi@yahoo.com>
2016-01-21 13:58:32 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,144658773,135137017,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,144658773,135137017,0,tonumber(yellow_hat},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
