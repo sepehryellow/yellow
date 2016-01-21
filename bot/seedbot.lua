d6d6281ca946ec3e9bc4cb267682604554c0deca
sepehryellow <sepehr.habashi@yahoo.com>
2016-01-21 13:57:03 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {110626080,144658773,135137017,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -230,10 +230,10 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@yellow_hat [Founder]
+@yellow_hat [Developer]
+@yellow_hat [Developer]
+@yellow_hat [Manager]
 
 Special thanks to
 awkward_potato
@@ -242,8 +242,8 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@yellow_hat [English]
+@yellow_hat [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
